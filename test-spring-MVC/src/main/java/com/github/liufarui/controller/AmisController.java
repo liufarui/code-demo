533ce7a595e645b0c5d5ca3c8d6f04d0704815aa
lfr-mac <liufr.dut@gmail.com>
2020-12-02 19:30:54 +0800

    amis

--- test-spring-MVC/src/main/java/com/github/liufarui/controller/AmisController.java
+++ test-spring-MVC/src/main/java/com/github/liufarui/controller/AmisController.java
@@ -2,11 +2,13 @@ package com.github.liufarui.controller;
 
 import com.github.liufarui.model.AmisResult;
 import com.google.gson.Gson;
+import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 /**
  * @author lfr
@@ -19,8 +21,33 @@ public class AmisController {
     @RequestMapping(method = RequestMethod.GET, value = "/menu")
     @ResponseBody
     @CrossOrigin(origins = "*")
-    public String amis(HttpServletRequest request, HttpServletResponse response) {
-        return "{\"$schema\":\"https://houtai.baidu.com/v2/schemas/page.json\",\"title\":\"应用管理系统\",\"toolbar\":[{\"type\":\"button\",\"actionType\":\"dialog\",\"label\":\"新增\",\"primary\":true,\"dialog\":{\"title\":\"新增\",\"body\":{\"type\":\"form\",\"name\":\"sample-edit-form\",\"api\":\"post:http://localhost:8088/amis\",\"controls\":[{\"type\":\"text\",\"name\":\"name\",\"label\":\"name\",\"required\":true},{\"type\":\"divider\"},{\"type\":\"text\",\"name\":\"sex\",\"label\":\"sex\",\"required\":true}]}}}],\"body\":{\"type\":\"crud\",\"draggable\":true,\"api\":\"http://localhost:8088/amis\",\"filter\":{\"title\":\"表属性搜索\",\"submitText\":\"\",\"controls\":[{\"type\":\"text\",\"name\":\"keywords\",\"placeholder\":\"通过属性名搜索\",\"addOn\":{\"label\":\"搜索\",\"type\":\"submit\"}},{\"type\":\"plain\",\"text\":\"这里的表单项可以配置多个\"}]},\"bulkActions\":[{\"label\":\"批量删除\",\"actionType\":\"ajax\",\"api\":\"delete:http://localhost:8088/amis/batch/delete/${ids}\",\"confirmText\":\"确定要批量删除?\"}],\"quickSaveApi\":\"http://localhost:8088/amis/update\",\"columns\":[{\"name\":\"id\",\"label\":\"id\",\"width\":20,\"sortable\":true,\"type\":\"text\",\"toggled\":true},{\"name\":\"name\",\"label\":\"name\",\"sortable\":true,\"searchable\":true,\"type\":\"text\",\"toggled\":true,\"quickEdit\":true},{\"name\":\"sex\",\"label\":\"sex\",\"sortable\":true,\"type\":\"text\",\"toggled\":true,\"quickEdit\":true},{\"type\":\"operation\",\"label\":\"操作\",\"width\":130,\"buttons\":[{\"type\":\"button\",\"icon\":\"fa fa-eye\",\"actionType\":\"dialog\",\"dialog\":{\"title\":\"查看\",\"body\":{\"type\":\"form\",\"controls\":[{\"type\":\"static\",\"name\":\"name\",\"label\":\"name\"},{\"type\":\"static\",\"name\":\"sex\",\"label\":\"sex\"},{\"type\":\"html\",\"html\":\"<p>添加其他 <span>Html 片段</span> 需要支持变量替换（todo）.</p>\"}]}}},{\"type\":\"button\",\"icon\":\"fa fa-pencil\",\"actionType\":\"dialog\",\"dialog\":{\"title\":\"编辑\",\"body\":{\"type\":\"form\",\"name\":\"sample-edit-form\",\"api\":\"http://localhost:8088/amis/$id\",\"controls\":[{\"type\":\"text\",\"name\":\"name\",\"label\":\"name\",\"required\":true},{\"type\":\"divider\"},{\"type\":\"text\",\"name\":\"sex\",\"label\":\"sex\",\"required\":true},{\"type\":\"divider\"}]}}},{\"type\":\"button\",\"icon\":\"fa fa-times text-danger\",\"actionType\":\"ajax\",\"confirmText\":\"您确认要删除?\",\"api\":\"delete:http://localhost:8088/amis/delete/$id\"}],\"toggled\":true}]},\"type\":\"page\"}";
+    public String amis(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String aaa =geti();
+//        return aaa;
+
+        return "{\"data\":{\"id\":1,\"name\":\"Issue CRUD\",\"schema\":\"{\\\"$schema\\\":\\\"https://houtai.baidu.com/v2/schemas/page.json\\\",\\\"title\\\":\\\"Issue CRUD\\\",\\\"toolbar\\\":[{\\\"type\\\":\\\"button\\\",\\\"actionType\\\":\\\"dialog\\\",\\\"label\\\":\\\"新增\\\",\\\"primary\\\":true,\\\"dialog\\\":{\\\"title\\\":\\\"新增\\\",\\\"body\\\":{\\\"type\\\":\\\"form\\\",\\\"name\\\":\\\"sample-edit-form\\\",\\\"api\\\":\\\"post:http://localhost:8088/amis/\\\",\\\"controls\\\":[{\\\"type\\\":\\\"text\\\",\\\"name\\\":\\\"标题\\\",\\\"label\\\":\\\"标题\\\"},{\\\"type\\\":\\\"text\\\",\\\"name\\\":\\\"紧急程度\\\",\\\"label\\\":\\\"紧急程度\\\"},{\\\"type\\\":\\\"text\\\",\\\"name\\\":\\\"需求描述\\\",\\\"label\\\":\\\"需求描述\\\"},{\\\"type\\\":\\\"text\\\",\\\"name\\\":\\\"需求类型\\\",\\\"label\\\":\\\"需求类型\\\"}]}}}],\\\"body\\\":{\\\"type\\\":\\\"crud\\\",\\\"draggable\\\":true,\\\"api\\\":\\\"http://localhost:8000/api/v0.1/data/Model/3/Row\\\",\\\"filter\\\":{\\\"title\\\":\\\"表属性搜索\\\",\\\"submitText\\\":\\\"\\\",\\\"controls\\\":[{\\\"type\\\":\\\"text\\\",\\\"name\\\":\\\"keywords\\\",\\\"placeholder\\\":\\\"通过属性名搜索\\\",\\\"addOn\\\":{\\\"label\\\":\\\"搜索\\\",\\\"type\\\":\\\"submit\\\"}},{\\\"type\\\":\\\"plain\\\",\\\"text\\\":\\\"这里的表单项可以配置多个\\\"}]},\\\"bulkActions\\\":[{\\\"label\\\":\\\"批量删除\\\",\\\"actionType\\\":\\\"ajax\\\",\\\"api\\\":\\\"delete:http://localhost:8088/amis/batch/delete/${ids}\\\",\\\"confirmText\\\":\\\"确定要批量删除?\\\"}],\\\"quickSaveApi\\\":\\\"http://localhost:8088/amis/update\\\",\\\"columns\\\":[{\\\"name\\\":\\\"标题\\\",\\\"label\\\":\\\"标题\\\",\\\"width\\\":20,\\\"sortable\\\":true,\\\"type\\\":\\\"text\\\",\\\"toggled\\\":true},{\\\"name\\\":\\\"紧急程度\\\",\\\"label\\\":\\\"紧急程度\\\",\\\"width\\\":20,\\\"sortable\\\":true,\\\"type\\\":\\\"text\\\",\\\"toggled\\\":true},{\\\"name\\\":\\\"需求描述\\\",\\\"label\\\":\\\"需求描述\\\",\\\"width\\\":20,\\\"sortable\\\":true,\\\"type\\\":\\\"text\\\",\\\"toggled\\\":true},{\\\"name\\\":\\\"需求类型\\\",\\\"label\\\":\\\"需求类型\\\",\\\"width\\\":20,\\\"sortable\\\":true,\\\"type\\\":\\\"text\\\",\\\"toggled\\\":true},{\\\"type\\\":\\\"operation\\\",\\\"label\\\":\\\"操作\\\",\\\"width\\\":130,\\\"buttons\\\":[{\\\"type\\\":\\\"button\\\",\\\"icon\\\":\\\"fa fa-eye\\\",\\\"actionType\\\":\\\"dialog\\\",\\\"dialog\\\":{\\\"title\\\":\\\"查看\\\",\\\"body\\\":{\\\"type\\\":\\\"form\\\",\\\"controls\\\":[{\\\"type\\\":\\\"static\\\",\\\"name\\\":\\\"name\\\",\\\"label\\\":\\\"name\\\"},{\\\"type\\\":\\\"divider\\\"},{\\\"type\\\":\\\"static\\\",\\\"name\\\":\\\"type\\\",\\\"label\\\":\\\"type\\\"},{\\\"type\\\":\\\"text\\\",\\\"name\\\":\\\"indexed\\\",\\\"label\\\":\\\"indexed\\\"},{\\\"type\\\":\\\"divider\\\"},{\\\"type\\\":\\\"divider\\\"},{\\\"type\\\":\\\"html\\\",\\\"html\\\":\\\"<p>添加其他 <span>Html 片段</span> 需要支持变量替换（todo）.</p>\\\"}]}}},{\\\"type\\\":\\\"button\\\",\\\"icon\\\":\\\"fa fa-pencil\\\",\\\"actionType\\\":\\\"dialog\\\",\\\"dialog\\\":{\\\"title\\\":\\\"编辑\\\",\\\"body\\\":{\\\"type\\\":\\\"form\\\",\\\"name\\\":\\\"sample-edit-form\\\",\\\"api\\\":\\\"http://localhost:8088/amis/$id\\\",\\\"controls\\\":[{\\\"type\\\":\\\"text\\\",\\\"name\\\":\\\"name\\\",\\\"label\\\":\\\"name\\\",\\\"required\\\":true},{\\\"type\\\":\\\"divider\\\"},{\\\"type\\\":\\\"text\\\",\\\"name\\\":\\\"type\\\",\\\"label\\\":\\\"type\\\",\\\"required\\\":true},{\\\"type\\\":\\\"divider\\\"}]}}},{\\\"type\\\":\\\"button\\\",\\\"icon\\\":\\\"fa fa-times text-danger\\\",\\\"actionType\\\":\\\"ajax\\\",\\\"confirmText\\\":\\\"您确认要删除?\\\",\\\"api\\\":\\\"delete:http://localhost:8088/amis/delete/$id\\\"}],\\\"toggled\\\":true}]},\\\"type\\\":\\\"page\\\"}\"}}";
+//        return "{\"$schema\":\"https://houtai.baidu.com/v2/schemas/page.json\",\"title\":\"应用管理系统\",\"toolbar\":[{\"type\":\"button\",\"actionType\":\"dialog\",\"label\":\"新增\",\"primary\":true,\"dialog\":{\"title\":\"新增\",\"body\":{\"type\":\"form\",\"name\":\"sample-edit-form\",\"api\":\"post:http://localhost:8088/amis\",\"controls\":[{\"type\":\"text\",\"name\":\"name\",\"label\":\"name\",\"required\":true},{\"type\":\"divider\"},{\"type\":\"text\",\"name\":\"sex\",\"label\":\"sex\",\"required\":true}]}}}],\"body\":{\"type\":\"crud\",\"draggable\":true,\"api\":\"http://localhost:8088/amis\",\"filter\":{\"title\":\"表属性搜索\",\"submitText\":\"\",\"controls\":[{\"type\":\"text\",\"name\":\"keywords\",\"placeholder\":\"通过属性名搜索\",\"addOn\":{\"label\":\"搜索\",\"type\":\"submit\"}},{\"type\":\"plain\",\"text\":\"这里的表单项可以配置多个\"}]},\"bulkActions\":[{\"label\":\"批量删除\",\"actionType\":\"ajax\",\"api\":\"delete:http://localhost:8088/amis/batch/delete/${ids}\",\"confirmText\":\"确定要批量删除?\"}],\"quickSaveApi\":\"http://localhost:8088/amis/update\",\"columns\":[{\"name\":\"id\",\"label\":\"id\",\"width\":20,\"sortable\":true,\"type\":\"text\",\"toggled\":true},{\"name\":\"name\",\"label\":\"name\",\"sortable\":true,\"searchable\":true,\"type\":\"text\",\"toggled\":true,\"quickEdit\":true},{\"name\":\"sex\",\"label\":\"sex\",\"sortable\":true,\"type\":\"text\",\"toggled\":true,\"quickEdit\":true},{\"type\":\"operation\",\"label\":\"操作\",\"width\":130,\"buttons\":[{\"type\":\"button\",\"icon\":\"fa fa-eye\",\"actionType\":\"dialog\",\"dialog\":{\"title\":\"查看\",\"body\":{\"type\":\"form\",\"controls\":[{\"type\":\"static\",\"name\":\"name\",\"label\":\"name\"},{\"type\":\"static\",\"name\":\"sex\",\"label\":\"sex\"},{\"type\":\"html\",\"html\":\"<p>添加其他 <span>Html 片段</span> 需要支持变量替换（todo）.</p>\"}]}}},{\"type\":\"button\",\"icon\":\"fa fa-pencil\",\"actionType\":\"dialog\",\"dialog\":{\"title\":\"编辑\",\"body\":{\"type\":\"form\",\"name\":\"sample-edit-form\",\"api\":\"http://localhost:8088/amis/$id\",\"controls\":[{\"type\":\"text\",\"name\":\"name\",\"label\":\"name\",\"required\":true},{\"type\":\"divider\"},{\"type\":\"text\",\"name\":\"sex\",\"label\":\"sex\",\"required\":true},{\"type\":\"divider\"}]}}},{\"type\":\"button\",\"icon\":\"fa fa-times text-danger\",\"actionType\":\"ajax\",\"confirmText\":\"您确认要删除?\",\"api\":\"delete:http://localhost:8088/amis/delete/$id\"}],\"toggled\":true}]},\"type\":\"page\"}";
+    }
+
+    @Test
+    public void test () throws IOException {
+        String aaa  = geti();
+        int bb =0;
+    }
+
+    private String geti() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                new FileInputStream("/Users/lfr/Documents/aaa")));
+
+        StringBuffer sb = new StringBuffer();
+
+        for (String line = br.readLine(); line != null; line = br.readLine()) {
+            sb.append(line);
+        }
+        br.close();
+
+        return sb.toString();
     }
 
 
