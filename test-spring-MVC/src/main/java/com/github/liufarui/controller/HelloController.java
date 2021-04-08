805e291d7e43de3ca6d7df2a5e4b7490d3aa8e12
liufarui <liufr.dut@gmail.com>
2021-04-08 23:51:48 +0800

    2021-04-08 23:51:26 星期四 14-week 098-day

--- test-spring-MVC/src/main/java/com/github/liufarui/controller/HelloController.java
+++ test-spring-MVC/src/main/java/com/github/liufarui/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.github.liufarui.controller;
 
+import com.github.liufarui.model.BeanT;
+import com.github.liufarui.model.BeanT2;
+import com.github.liufarui.model.BeanTT;
 import com.github.liufarui.model.ModelT;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,10 +18,22 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping(value = "/hello")
 @CrossOrigin(origins = "*")
 public class HelloController {
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     @ResponseBody
     @CrossOrigin(origins = "*")
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+
+        // BeanT2 t2 = new BeanT2("123");
+        // BeanT t = new BeanT();
+        // Jackson2ObjectMapperBuilder.json();
+        // ObjectMapper mapper = new ObjectMapper();
+        //
+        // //A.javaBean---->JSON字符串
+        // //1.writeValueAsString
+        // String json1 = mapper.writeValueAsString(t);
+
+        // BeanUtils.copyProperties(t2, t);
+        return new BeanTT().toString();
 
 //        //创建ModelAndView对象
 //        ModelAndView mav = new ModelAndView();
@@ -29,7 +43,7 @@ public class HelloController {
 //        mav.setViewName("/WEB-INF/jsp/hello.jsp");
 //        //返回ModelAndView对象
 //        return mav;
-        return "HAHA";
+//         return "HAHA";
     }
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
---
+++ test-spring-MVC/src/main/java/com/github/liufarui/model/BeanTT.java
@@ -0,0 +1,23 @@
+package com.github.liufarui.model;
+
+/**
+ * @author liufarui
+ * @Description:
+ * @date 2021/4/8 9:07 下午
+ */
+public class BeanTT {
+    private String a;
+    private String c;
+
+    public String getX() {
+        return "asd";
+    }
+
+    @Override
+    public String toString() {
+        return "BeanTT{" +
+                "a='" + a + '\'' +
+                ", c='" + c + '\'' +
+                '}';
+    }
+}
